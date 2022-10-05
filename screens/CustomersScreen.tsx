2879f4ca47d6cc7e88a2a84fa5526f14882f8314
Dovydas <telesoftas@Dovydas-MacBook-Pro.local>
2022-10-05 19:11:39 +0300

    Started app, added navigation and customer screen

---
+++ screens/CustomersScreen.tsx
@@ -0,0 +1,46 @@
+import { View, Text, SafeAreaView, ScrollView } from "react-native";
+import React, { useLayoutEffect, useState } from "react";
+import { useTailwind } from "tailwind-rn/dist";
+import {
+  CompositeNavigationProp,
+  useNavigation
+} from "@react-navigation/native";
+import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
+import { TabNavigatorParamList } from "../navigator/TabNavigator";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../navigator/RootNavigator";
+import { Image, Input } from "@rneui/themed";
+
+export type CustomerScreenNavigationProp = CompositeNavigationProp<
+  BottomTabNavigationProp<TabNavigatorParamList, "Customers">,
+  NativeStackNavigationProp<RootStackParamList>
+>;
+
+const CustomersScreen = () => {
+  const tw = useTailwind();
+  const navigation = useNavigation<CustomerScreenNavigationProp>();
+  const [searchInput, setSearchInput] = useState<string>("");
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false
+    });
+  }, []);
+
+  return (
+    <ScrollView style={{ backgroundColor: "#59C1CC" }}>
+      <Image
+        source={{ uri: "https://links.papareact.com/3jc" }}
+        containerStyle={tw("w-full h-64")}
+      />
+      <Input
+        placeholder="Search by Customer"
+        value={searchInput}
+        onChangeText={setSearchInput}
+        containerStyle={tw("bg-white pt-5 pb-0 px-10")}
+      />
+    </ScrollView>
+  );
+};
+
+export default CustomersScreen;
